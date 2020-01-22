b705ffe80a830adea3ca61b5771393bab175ba21
mani006006 <mani006006@gmail.com>
2020-01-22 22:10:02 +0530

    Create __manifest__.py

---
+++ hr_attendance_update/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Attendance Update',
+    'version': '1.0',
+    'summary': 'Customize the modules',
+    'description': """Attendance Update""",
+    'depends': ['hr_attendance'],
+    'data': [
+        'views/hr_attendance_shift.xml',
+    ],
+}
