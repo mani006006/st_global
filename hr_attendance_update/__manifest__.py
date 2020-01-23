25cb34a7a368119b69aff0b66b536a28e967a40d
mani006006 <mani006006@gmail.com>
2020-01-23 18:17:24 +0530

    Add files via upload

--- hr_attendance_update/__manifest__.py
+++ hr_attendance_update/__manifest__.py
@@ -6,8 +6,14 @@
     'version': '1.0',
     'summary': 'Customize the modules',
     'description': """Attendance Update""",
-    'depends': ['hr_attendance'],
+    'depends': ['hr_attendance','hr','mail'],
     'data': [
+        'security/ir.model.access.csv',
         'views/hr_attendance_shift.xml',
+        'views/hr_attendance.xml',
+        'views/hr_view.xml',
+        'wizard/non_checkout.xml',
+        'views/check_out_cron.xml',
+        'views/email_template.xml',
     ],
 }
